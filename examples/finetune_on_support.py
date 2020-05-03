4b9289b40a66554be7360170c825f302d68c3454
Sanil Pande <46553192+sanilpande@users.noreply.github.com>
2020-05-02 20:04:04 -0400

    added training

--- examples/finetune_on_support.py
+++ examples/finetune_on_support.py
@@ -1,12 +1,26 @@
 from triplet_loss import triplet_loss
-import torch
+import torch.nn as nn
 
-def finetune_on_support(model, Dataloader, orig_target_dict):
-    for batch_id, data in enumerate(Dataloader):
-        target_old_task = orig_target_dict(data['paths'])
-        orig_feats, new_feats = model(data['data'])
-        labels = data['labels']
+def finetune_on_support(model, Dataloader, orig_target_dict, epochs=10, lr=0.001, logger=None):
+    MSE = nn.MSELoss()
+    optimizer = nn.optim.AdamW(model.parameters(), lr=lr)
+    
+    for epoch in range(epochs)
+        for batch_id, data in enumerate(Dataloader):
 
-        L_old = MSE(orig_feats,target_old_task)
-        L_new = triplet_loss(new_feats, labels)
-        L_total = L_old+L_new
+            optimizer.zero_grad()
+
+            target_old_task = orig_target_dict(data['paths'])
+            orig_feats, new_feats = model(data['data'])
+            labels = data['labels']
+
+            L_old = MSE(orig_feats,target_old_task)
+            L_new = triplet_loss(new_feats, labels)
+            L_total = L_old + L_new
+            
+            if logger is not None:
+                logger.add_scalar("loss/iter", L_total.item(), epoch*len(Dataloader)+batch_id)
+            
+            L_total.backward()
+            optimizer.step()
+    
